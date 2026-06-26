372b65e5d7c3fad9bb36c3099b8daa67565f3747
fidel208 <dempafidel@gmail.com>
2026-06-26 06:24:51 +0300

    header

--- Frontend/src/components/header/Header.jsx
+++ Frontend/src/components/header/Header.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div className="header">
       <h1>MWITIX NET</h1>
       <div className="others">
         <p>Hello Fidel</p>
-        <span id="dp">
+        <span id="dp" onClick={toggleDropdown}>
           <span id="online"></span>
         </span>
-        <span className="dropdown">
-          <a href="">Account settings</a>
-          <a href="/login">Log out</a>
-        </span>
+        {isOpen && (
+          <span className="dropdown">
+            <a href="#">
+              <i class="fa-solid fa-user"></i>Account settings
+            </a>
+            <a href="/login">
+              {" "}
+              <i class="fa-solid fa-arrow-right-from-bracket"></i>Log out
+            </a>
+          </span>
+        )}
       </div>
     </div>
   );
